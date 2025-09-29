f48f2a8cee6ce92c8737efab54543659cc4155be
ErrGuhan <mguhan6383@gmail.com>
2025-09-29 23:16:02 +0530

    Optimize codebase: add .gitignore, prep for deployment

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,10 +1,10 @@
 require('@nomiclabs/hardhat-ethers');
 module.exports = {
   solidity: '0.8.24',
-  networks: {
-    polygon_mumbai: {
-      url: process.env.BLOCKCHAIN_RPC_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
-    }
-  }
+  // networks: {
+  //   polygon_mumbai: {
+  //     url: process.env.BLOCKCHAIN_RPC_URL,
+  //     accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+  //   }
+  // }
 };
